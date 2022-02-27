af3607e37988c94fb7cf6844df6a8680d92875dc
Michal Kajzar <michal.kajzar@gmail.com>
2022-02-27 23:01:06 +0100

    Navigation bar with navigation

---
+++ lib/util/widgets/navigation_bar/navigation_bar_listener.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class NavigationBarListener {
+  void shopClicked(BuildContext context);
+  void upgradesClicked(BuildContext context);
+  void clothingClicked(BuildContext context);
+  void settingsClicked(BuildContext context);
+}
