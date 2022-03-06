e346221d0a9b71b3aa3835877e263b26a9c4b81f
Michal Kajzar <michal.kajzar@gmail.com>
2022-03-06 13:31:38 +0100

    Initializers and injectable logic change

--- lib/di/main_module.dart
+++ lib/di/main_module.dart
@@ -1,10 +1,5 @@
 import 'package:injectable/injectable.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class MainModule {
-
-  @singleton
-  @preResolve
-  Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 }
--- lib/initialize/initializer.dart
+++ lib/initialize/initializer.dart
@@ -1,14 +1,18 @@
 import 'package:alpaka_clicker/di/injector.dart';
 import 'package:alpaka_clicker/initialize/bank_initializer.dart';
+import 'package:alpaka_clicker/initialize/logger_initializer.dart';
+import 'package:alpaka_clicker/initialize/shared_prefs_initializer.dart';
 import 'package:get_it/get_it.dart';
 
 Future<void> initialize() async {
-  await configInjector(GetIt.instance);
  for (var initializer in [
-   BankInitializer()
+   SharedPrefsInitializer(),
+   BankInitializer(),
+   LoggerInitializer()
  ]) {
    await initializer();
  }
+ await configInjector(GetIt.instance);
 }
 
 abstract class Initializer {
