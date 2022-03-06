15e212aa12783976877b4cd275e5e9ce070d172c
Michal Kajzar <michal.kajzar@gmail.com>
2022-03-06 13:28:33 +0100

    New displayable money logic

--- lib/util/widgets/money_counters/money_counter.dart
+++ lib/util/widgets/money_counters/money_counter.dart
@@ -1,3 +1,4 @@
+import 'package:alpaka_clicker/util/widgets/disposable_widget.dart';
 import 'package:alpaka_clicker/util/widgets/money_counters/money_counter_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -20,24 +21,27 @@ class MoneyCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _controller.initializeMoneyUpdate();
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          padding: const EdgeInsets.only(right: 4),
-          child: Observer(
-            builder: (_) => Text(
-              _controller.actualMoney,
-              style: textStyle,
+    return DisposableWidget(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(right: 4),
+            child: Observer(
+              builder: (_) => Text(
+                _controller.actualMoney,
+                style: textStyle,
+              ),
             ),
           ),
-        ),
-        Image.asset(
-          "assets/images/kremowka.png",
-          height: 24,
-          width: 24,
-        )
-      ],
+          Image.asset(
+            "assets/images/kremowka.png",
+            height: 24,
+            width: 24,
+          )
+        ],
+      ),
+      dispose: () => _controller.dispose()
     );
   }
 }
