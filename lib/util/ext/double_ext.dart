033eefcf55e424c06488021c07b15cbc2905cb36
Michal Kajzar <michal.kajzar@gmail.com>
2022-04-26 09:09:10 +0200

    Ceil offer price so it is only natural number

--- lib/util/ext/double_ext.dart
+++ lib/util/ext/double_ext.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 extension DoubleExt on double {
   double toPrecision(int n) => double.parse(toStringAsFixed(n));
 
@@ -7,5 +9,13 @@ extension DoubleExt on double {
     final afterPoint = initialString.substring(1, initialString.length);
     final stringWithOnePlace = beforePoint + "." + afterPoint;
     return double.parse(stringWithOnePlace);
+  }
+
+  double trimAfterPoint({int place = 0}) {
+    final initialString = toString();
+    final correctedInitialCutIndex = initialString.indexOf(".") + 1;
+    final cutIndex = min(correctedInitialCutIndex + place, initialString.length);
+    final trimmedString = initialString.substring(0, cutIndex);
+    return double.parse(trimmedString);
   }
 }
